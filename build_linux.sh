0419283d7fa13ebafd39d0d3547e53a1a38b75d6
saucechef <saucechef@bytekitchen.de>
2020-08-27 18:02:11 +0200

    Testing build scripts.

--- build_linux.sh
+++ build_linux.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-mkdir build
-cd build
-cmake ..
-cmake --build .
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' "cmake"|grep "install ok installed")
+if [ "" = "$PKG_OK" ]
+then
+  echo "You need to install cmake first! (sudo apt install cmake)"
+else
+  mkdir build
+  cd build
+  cmake ..
+  cmake --build .
+fi
