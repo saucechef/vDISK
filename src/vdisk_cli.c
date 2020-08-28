a362c413ceadeebaf8f6f4d44cca08f44196a6a1
saucechef <saucechef@bytekitchen.de>
2020-08-28 13:31:24 +0200

    I dont even know.

--- src/vdisk_cli.c
+++ src/vdisk_cli.c
@@ -14,7 +14,7 @@ int main(int argc, char* argv[]) {
     if (argc != 3) {
         printf("\nFor command-line use, it is necessary to initially create or load a vDrive.\n\n");
         printf("Usage:\n\tvdisk_cli create <size of disk in bytes>/<sectors (512 byte) per block>\n");
-        printf("or\n\tvdisk_cli load <path of existing disk file (.vdisk)>\n");
+        printf("or\n\tvdisk_cli load <path of existing disk image (.img)>\n");
         printf("Example:\n\tvdisk_cli create 65536/4\nor\n\tvdisk_cli load drive1\n");
         return -1;
     }
@@ -38,6 +38,9 @@ int main(int argc, char* argv[]) {
     while(true) {
         break;
     }
+
+    // DEBUG //
+    printHexdump(drive, 0, 0, true);
 
     return 0;
 }
